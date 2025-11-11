4a9922e1268882122fcc3821a3b8963c3c66569b
danixylkhan <79591148+daniyxlkhan@users.noreply.github.com>
2025-11-11 11:46:51 -0400

    Add shorts side bar hider

--- scripts/youtube.js
+++ scripts/youtube.js
@@ -1,14 +1,32 @@
-function hideShorts() {
-    const shortsElements = document.querySelectorAll('a[href*="/shorts"]');
-    shortsElements.forEach((element) => {
-        element.style.display = 'none';
-    });
+function hideShortsSideTab() {
+    // hide the shorts side tab for large page size
+    const largePageSections = document.querySelectorAll('ytd-guide-entry-renderer');
+    largePageSections.forEach(section => {
+        const headingText = section.textContent;
+        if (headingText.includes('Shorts') && !section.hidden) {
+            console.log('Found Large Shorts tab by text:', section);
+            section.style.display = 'none';
+            section.hidden = true;
+        }
+    })
+
+    // hide the shorts side tab for smaller page size
+    const mediumPageSections = document.querySelectorAll('ytd-mini-guide-entry-renderer');
+    mediumPageSections.forEach(section => {
+        const headingText = section.textContent;
+        if (headingText.includes('Shorts') && !section.hidden) {
+            console.log('Found Medium Shorts tab by text:', section);
+            section.style.display = 'none';
+            section.hidden = true;
+        }
+    })
 }
 
 const observer = new MutationObserver(() => {
-    hideShorts();
+    hideShortsSideTab();
+
 });
 
 observer.observe(document.body, { childList: true, subtree: true });
 
-hideShorts();
+hideShortsSideTab();
